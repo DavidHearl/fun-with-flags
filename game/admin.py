c3fe034ccc9497e153e71ac829cf2ec60b61c15b
DavidHearl <david.hearl@outlook.com>
2022-08-04 12:05:27 +0100

    Added css and javascript

--- game/admin.py
+++ game/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Flag
 
 
-admin.site.register(Flag)
+@admin.register(Flag)
+class FlagAdmin(admin.ModelAdmin):
+    list_display = ('flag', 'programatic_name', 'image', 'fact', 'creation_date', 'difficulty')
+    prepopulated_fields = {'programatic_name': ('flag',)}
+
