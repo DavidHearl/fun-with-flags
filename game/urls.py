1e5139c3d3f6fc802e20053f630a7856ebf5e0b9
DavidHearl <david.hearl@outlook.com>
2022-08-04 16:59:17 +0100

    Added easy, medium, hard and extreme templates

--- game/urls.py
+++ game/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.game, name='game')
+    path('', views.game, name='game'),
+    path('easy/', views.game, name='easy'),
+    path('medium/', views.game, name='medium'),
+    path('hard/', views.game, name='hard'),
+    path('extreme/', views.game, name='extreme')
 ]
--- game/views.py
+++ game/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Flag
 
+
 def game(request):
 
     flags = Flag.objects.all()
@@ -10,3 +11,14 @@ def game(request):
     }
 
     return render(request, 'game/game.html', context)
+
+
+def easy(request):
+
+    flags = Flag.objects.all()
+
+    context = {
+        'flags': flags
+    }
+
+    return render(request, 'game/easy.html', context)
